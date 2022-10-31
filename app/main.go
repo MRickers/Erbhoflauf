9f40ead48ecb89d7b1bad8eca957f7db67ec393a
Max <max_rickers@hotmail.de>
2022-10-31 17:49:32 +0100

    Listening port as cli argment

--- app/main.go
+++ app/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	var listen_port uint64
+
+	flag.Uint64Var(&listen_port, "p", 5000, "Specify port to listen to")
+
+	flag.Parse()
+
 	http.Handle("/", http.FileServer(http.Dir("./app/static")))
 
-	log.Fatal(http.ListenAndServe(":80", nil))
+	var listener = ":" + strconv.FormatUint(listen_port, 10)
+	log.Printf("Listening on port %s", listener)
+	log.Fatal(http.ListenAndServe(listener, nil))
 }
